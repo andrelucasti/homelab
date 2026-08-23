5f3a19ec0f3d39daf4a1d5c3295fdf3986a83a16
André Lucas <andrelucastic@gmail.com>
2026-08-23 12:54:21 +0100

    create providers file and kubeconfig file

--- infra/main.tf
+++ infra/main.tf
@@ -1,28 +1,15 @@
-terraform {
-    required_providers {
-        multipass = {
-            source = "todoroff/multipass"
-        }
-
-    }
-}
-
 resource "random_password" "k3s_token" {
   length  = 48
   special = false
 }
 
-provider "multipass" {
-    
-}
-
 resource "multipass_instance" "k8s-master" {
-    name = "k8s-master"
-    cpus = "2"
-    memory = "4G"
-    image  = "24.04"
-    disk = "20G"
-    cloud_init = templatefile("${path.module}/cloud_init/master.yaml.tpl",{
+    name        = "k8s-master"
+    cpus        = "2"
+    memory      = "4G"
+    image       = "24.04"
+    disk        = "20G"
+    cloud_init  = templatefile("${path.module}/cloud_init/master.yaml.tpl",{
         k3s_token         = random_password.k3s_token.result
         tailscale_authkey = var.tailscale_authkey
 
@@ -32,22 +19,45 @@ resource "multipass_instance" "k8s-master" {
 }
 
 resource "multipass_instance" "k8s-worker" {
-    count = 3
-    name = "k8s-worker-${count.index}"
-    cpus = "2"
-    memory = "4G"
-    disk = "20G"
-    cloud_init = templatefile("${path.module}/cloud_init/worker.yaml.tpl",{
+    count       = 3
+    name        = "k8s-worker-${count.index}"
+    cpus        = "2"
+    image       = "24.04"
+    memory      = "4G"
+    disk        = "20G"
+    cloud_init  = templatefile("${path.module}/cloud_init/worker.yaml.tpl",{
         master_ip         = multipass_instance.k8s-master.ipv4[0]
         k3s_token         = random_password.k3s_token.result
         tailscale_authkey = var.tailscale_authkey
 
     })
 
-    depends_on = [ multipass_instance.k8s-master ]
+    depends_on  = [ multipass_instance.k8s-master ]
   
 }
 
 output "master_ip" {
     value = multipass_instance.k8s-master.ipv4[0]
+}
+
+data "external" "kubeconfig" {
+    depends_on = [ multipass_instance.k8s-master ]
+    program = ["bash", "-c", <<-EOT
+    set -euo pipefail
+
+    TS_IP=$$(multipass exec k8s-master -- tailscale ip -4 | tr -d '\r\n')
+    [ -n "$$TS_IP" ] || { echo "tailscale nao retornou IPv4 no master" >&2; exit 1; }
+
+    KC=$$(multipass exec k8s-master -- sudo cat /etc/rancher/k3s/k3s.yaml \
+          | sed "s#127\.0\.0\.1#$$TS_IP#")
+
+    jq -n --arg raw "$$KC" '{raw: $$raw}'
+  EOT
+  ]
+}
+
+resource "local_sensitive_file" "kubeconfig" {
+  content           = data.external.kubeconfig.result.raw
+  filename          = "${path.module}/kubeconfig.yaml"
+  file_permission   = "0600"
 }
